7ca20eb188f656354e51770349b365be9101e784
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-10-25 14:20:47 +0200

    rewrite user to use signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,14 +10,26 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  //With signals approach:
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+
+  //Way no signals:
+  /*
   selectedUser = DUMMY_USERS[randomIndex];
 
   get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar;
   }
 
-  onSelectuser() {
+  onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser = DUMMY_USERS[randomIndex];
   }
+  */
 }
