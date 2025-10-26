04adc1fb17cda3753c1f4d94006b3caec85c8a4f
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-10-26 11:56:23 +0100

    add complete tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, input } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
-import { dummyTasks } from '../dummy-tasks';
 import { User } from '../user.interface';
 
 @Component({
@@ -18,5 +17,34 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.user.id);
   }
 
-  tasks = dummyTasks;
+  onCompleteTask(taskId: string) {
+    //find and delete task from list
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
 }
