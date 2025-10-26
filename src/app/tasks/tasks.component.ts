48f7781919360c372f4b629d9c46ec7226a087d0
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-10-26 13:54:06 +0100

    add cancel behavior in dialog

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input, input } from '@angular/core';
-import { TaskComponent } from '../task/task.component';
+import { TaskComponent } from './task/task.component';
 import { User } from '../user.interface';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input() user!: User;
+  isAddingTask = false;
 
   //Filter tasks for specific user -> done with getter (or in signals a computed value )
   get selectedUserTasks() {
@@ -20,6 +22,14 @@ export class TasksComponent {
   onCompleteTask(taskId: string) {
     //find and delete task from list
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 
   tasks = [
